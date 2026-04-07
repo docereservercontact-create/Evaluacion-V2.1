fcd1e41929e85177d01d6c1c347a68cfcc45552a
docereservercontact-create <docereservercontact@gmail.com>
2026-04-07 01:02:13 -0600

    Create examen2.js

---
+++ exams/examen2.js
@@ -0,0 +1,20 @@
+const examen2 = {
+    title: "EXAMEN DE PRUEBA 2",
+    questions: [
+        {
+            q: "¿Cuánto es 2+2?",
+            o: ["3","4","5","6"],
+            c: 1
+        },
+        {
+            q: "Capital de México",
+            o: ["GDL","CDMX","MTY","PUE"],
+            c: 1
+        },
+        {
+            q: "5 x 3",
+            o: ["15","10","20","8"],
+            c: 0
+        }
+    ]
+};
